68b30e53e1f3fa1e18d1de7c551eba4965a9777c
Bootcamp <bootcamp@Abduls-MacBook-Air.local>
2023-06-11 04:25:18 -0700

    Footer added

---
+++ client/src/pages/Categories.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import { useParams } from 'react-router-dom';
+
+// function CategoryPage() {
+//   const { id } = useParams();
+
+
+//   return (
+//     <div>
+//       <h2>Category Page</h2>
+//       <p>Category ID: {id}</p>
+//       {/* Add your category-specific content here */}
+//     </div>
+//   );
+// }
+
+// export default CategoryPage;
